c14f5567ab33463f9c7a0ead9b8f9a26a8d0249c
ufuckee <vip@pornhub.cpm>
2021-11-08 13:33:59 +0800

    update

--- xdd.go
+++ xdd.go
@@ -5,9 +5,11 @@ import "github.com/beego/beego/v2/client/httplib"
 //对接xdd
 func xdd(cookie string, qq string) {
 	xdd_url := jd_cookie.Get("xdd_url")
+	xdd_token := jd_cookie.Get("xdd_token")
 	if xdd_url != "" {
 		req := httplib.Post(xdd_url)
 		req.Param("ck", cookie)
+		req.Param("token", xdd_token)
 		req.Param("qq", qq)
 		req.Response()
 	}
