6392e16883af48cfc8fc5cfc12aafee1c7358c92
ufuckee <vip@pornhub.cpm>
2021-11-14 17:15:25 +0800

    update

--- init.go
+++ init.go
@@ -3,9 +3,14 @@ package jd_cookie
 import (
 	"os"
 	"strings"
+
+	"github.com/cdle/sillyGirl/core"
 )
 
 func init() {
+	if !core.Bucket("qinglong").GetBool("enable_qinglong", true) {
+		return
+	}
 	data, _ := os.ReadFile("dev.go")
 	if !strings.Contains(string(data), "jd_cookie") && !jd_cookie.GetBool("enable_jd_cookie") {
 		return
