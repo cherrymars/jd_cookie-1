d204bc9685344af223035531bb9d60b923329833
ufuckee <vip@pornhub.cpm>
2021-12-21 16:53:04 +0800

    update

--- init.go
+++ init.go
@@ -1,10 +1,14 @@
 package jd_cookie
 
 import (
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 
+	"github.com/beego/beego/v2/core/logs"
 	"github.com/cdle/sillyGirl/core"
+	"golang.org/x/net/proxy"
 )
 
 func init() {
@@ -24,4 +28,42 @@ func init() {
 	initSubmit()
 	initTyt()
 	initNotify()
+	buildHttpTransportWithProxy()
+}
+
+var Transport *http.Transport
+
+func buildHttpTransportWithProxy() {
+	addr := jdNotify.Get("http_proxy")
+	if strings.Contains(addr, "http://") {
+		if addr != "" {
+			u, err := url.Parse(addr)
+			if err != nil {
+				logs.Warn("can't connect to the http proxy:", err)
+				return
+			}
+			Transport = &http.Transport{Proxy: http.ProxyURL(u)}
+		}
+	}
+	if strings.Contains(addr, "sock5://") || strings.Contains(addr, "socks5://") {
+		addr = strings.Replace(addr, "sock5://", "", -1)
+		addr = strings.Replace(addr, "socks5://", "", -1)
+		var auth *proxy.Auth
+		v := strings.Split(addr, "@")
+		if len(v) == 3 {
+			auth = &proxy.Auth{
+				User:     v[1],
+				Password: v[2],
+			}
+			addr = v[0]
+		}
+		dialer, err := proxy.SOCKS5("tcp", addr, auth, proxy.Direct)
+		if err != nil {
+			logs.Warn("can't connect to the sock5 proxy:", err)
+			return
+		}
+		Transport = &http.Transport{
+			Dial: dialer.Dial,
+		}
+	}
 }
