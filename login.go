46f436659ab03eb133c5469c0a0aff490fbf095f
ufuckee <vip@pornhub.cpm>
2022-01-09 13:23:53 +0800

    update

--- login.go
+++ login.go
@@ -96,6 +96,7 @@ func initLogin() {
 				hasNolan := false
 				// ke := core.Bucket("wxmp").GetBool("isKe?", false)
 				data, err := httplib.Get(addr + "/api/Config").Bytes()
+				logs.Info(string(data))
 				if err != nil && s.IsAdmin() {
 					return err
 				}
